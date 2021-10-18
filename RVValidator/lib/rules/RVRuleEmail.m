b484ff0eb9e091be58d8ccb11771a3cfcbeca7a6
aleixgilaguilar <aleix.gil@revo.works>
2021-10-18 17:41:04 +0200

    Strict email validation. (no spaces and email format xxx@xxx.xxx). Tag to 1.2.5.

--- RVValidator/lib/rules/RVRuleEmail.m
+++ RVValidator/lib/rules/RVRuleEmail.m
@@ -12,11 +12,8 @@
 
 -(BOOL)performValidation:(NSString*)text{
     if( [self isEmptyString:text] ) return true;
-    
-    BOOL stricterFilter     = NO; // Discussion http://blog.logichigh.com/2010/09/02/validating-an-e-mail-address/
-    NSString *stricterFilterString = @"^[A-Z0-9a-z\\._%+-]+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2,4}$";
-    NSString *laxString     = @"^.+@([A-Za-z0-9-]+\\.)+[A-Za-z]{2}[A-Za-z]*$";
-    NSString *emailRegex    = stricterFilter ? stricterFilterString : laxString;
+
+    NSString *emailRegex    = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}";
     NSPredicate *emailTest  = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailRegex];
     
     BOOL valid = [emailTest evaluateWithObject:text];
